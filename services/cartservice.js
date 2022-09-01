e828dec105910129ab5bafc615a14958d4056307
baekdae <baekdae89@gmail.com>
2022-09-01 15:12:51 +0900

    Revert "/cart post api 추가"
    This reverts commit b6920613481abfc740a661083f5b1ec755a46772.

--- services/cartservice.js
+++
@@ -1,7 +0,0 @@
-const cart = require("../models/cartdao");
-
-const createCart = async (userId, productId, productAmount, paymentAmount) => {
-  return await cart.createCart(userId, productId, productAmount, paymentAmount)
-}
-
-module.exports = { createCart };
