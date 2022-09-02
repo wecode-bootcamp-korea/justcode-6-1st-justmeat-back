c5860521dfe0461894058b3922fbf6341c663993
연쁜 <yeon@yeonppeun-ui-MacBookPro.local>
2022-09-02 10:59:13 +0900

    cart CRUD commit conflict

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const userrouter = require("../routes/userrouter");
 const productrouter = require('./productrouter');
-const cartrouter = require("../routes/cartroute");
+const cartrouter = require("../routes/cartrouter");
+const salerouter = require("../routes/salerouter");
 
 router.use('/user', "/", userrouter);
 router.use("/product", productrouter);
 router.use('/cart', cartrouter);
+router.use('/sale', salerouter);
 
 module.exports = router;
