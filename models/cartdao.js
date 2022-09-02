f683a0dcdb7f18b9c1736c00998cdc9a86e57753
연쁜 <yeon@yeonppeun-ui-MacBookPro.local>
2022-09-02 17:13:52 +0900

    cart CRUD commit

--- models/cartdao.js
+++ models/cartdao.js
@@ -20,19 +20,19 @@ myDataSource
   });
 
 
-const createCart = async (userId, productId, productAmount, paymentAmount) => {
+const createCart = async (userId, productId, productAmount, productPrice) => {
   return await myDataSource.query(`
   INSERT INTO cart_lists (userId, productId, productAmount, paymentAmount)
   VALUES(?,?,?,?)
-  `, [userId, productId, productAmount, paymentAmount]);
+  `, [userId, productId, productAmount, productAmount * productPrice]);
 }
 
-const updateCart = async (userId, productId, productAmount, paymentAmount) => {
+const updateCart = async (userId, productId, productAmount, productPrice) => {
   const cart = await myDataSource.query(`
   UPDATE cart_lists
   SET productAmount = ?, paymentAmount = ?
   WHERE userId = ? AND productId = ?;
-  `, [productAmount, paymentAmount, userId, productId]);
+  `, [productAmount, productAmount * productPrice, userId, productId]);
   return cart;
 }
 
@@ -43,22 +43,52 @@ const deleteCart = async (pk) => {
   `, [pk])
 }
 
+// const readCart = async (userId) => {
+//   const GETcart = await myDataSource.query(`
+//   SELECT
+//   cart_lists.userId,
+//   JSON_ARRAYAGG(
+//     JSON_OBJECT(
+//       'productId', cart_lists.productId,
+//       'productAmount', cart_lists.productAmount,
+//       'paymentAmount', cart_lists.paymentAmount
+//     )
+//   ) as cart
+//   FROM justmeat.cart_lists
+//   WHERE userId = ?
+//   GROUP BY userId
+//   `, [userId])
+//   return GETcart;
+// }
+
+// const readCart = async (req, res) => {
+//   const GETcart = await myDataSource.query(`
+//   SELECT
+//   cart_lists.productId as productId,
+//   cart_lists.userId as userId,
+//   products.productImgbyId as productImg,
+//   products.productName as productName,
+//   products.productOption as width,
+//   cart_lists.productAmount as productAmount,
+//   products.price as productPrice,
+//   cart_lists.paymentAmount as paymentAmount
+//   FROM justmeat.cart_lists
+//   JOIN justmeat.products ON products.categoryId = cart_lists.productId;
+//   `)
+//   return GETcart;
+// }
+
+// const readCart = async (userId) => {
+//   const GETcart = await myDataSource.query(`
+//   select * from cart_lists as t1 
+//   INNER JOIN product as t2 on t1.productId = t2.id where t1.userId = ?;
+
+//   `, [userId])
+//   return GETcart;
+// }
 const readCart = async (userId) => {
   const GETcart = await myDataSource.query(`
-  SELECT
-  cart_lists.userId,
-  JSON_ARRAYAGG(
-    JSON_OBJECT(
-      'productId', cart_lists.productId,
-      'productAmount', cart_lists.productAmount,
-      'paymentAmount', cart_lists.paymentAmount
-    )
-  ) as cart
-  FROM justmeat.cart_lists
-  WHERE userId = ?
-  GROUP BY userId
-  `, [userId])
-  return GETcart;
+  select * from cart_lists as t1 INNER JOIN product as t2 on t1.productId = t2.id where t1.userId = ?;`, [userId])
 }
 
 // 추가추가
@@ -70,7 +100,7 @@ const checkCart = async (userId, productId, productAmount, paymentAmount) => {
   `, [userId, productId]);
 
   // cart_lists에 값이 담겨져 있으면 updateCart(); 실행, 값이 없으면 createCart();
-  return cart ? await updateCart(userId, productId, productAmount, paymentAmount) : await createCart(userId, productId, productAmount, paymentAmount);
+  return cart.length > 0 ? await updateCart(userId, productId, productAmount, productPrice) : await createCart(userId, productId, productAmount, p);
 }
 
 module.exports = { createCart, updateCart, deleteCart, readCart, checkCart };
